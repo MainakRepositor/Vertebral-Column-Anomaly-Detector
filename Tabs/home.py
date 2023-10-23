f2f6916e6191c64ec37020d45592ec391d9e9697
MAINAK CHAUDHURI <sultankhilji001@gmail.com>
2023-10-23 17:29:22 +0530

    Update home.py

--- Tabs/home.py
+++ Tabs/home.py
@@ -7,7 +7,7 @@ def app():
     """This function create the home page"""
     
     # Add title to the home page
-    st.title("Concrete Strength Detection System")
+    st.title("Vertebral Column Anomaly Detection System")
 
     # Add image to the home page
     st.image("./images/home.png")
